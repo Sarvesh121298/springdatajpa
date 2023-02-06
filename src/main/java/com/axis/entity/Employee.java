48520b18c9b4ed1af63061a9147cb90c97de8c49
sarvesh12 <sarveshsawant1212@gmail.com>
2023-02-06 10:05:50 +0530

    first commit

---
+++ src/main/java/com/axis/entity/Employee.java
@@ -0,0 +1,54 @@
+package com.axis.entity;
+
+//import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Employee {
+	@Id
+	private int id;
+	private String name;
+	private int salary;
+	private String dept;
+	private int age;
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	
+
+}
